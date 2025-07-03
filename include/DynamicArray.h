38fa041363cdeb264607b107400c11e5420061ae
dudukin matvei <matduduk@gmail.com>
2025-07-03 04:48:37 +0300

    completed DynamicArray

--- include/DynamicArray.h
+++ include/DynamicArray.h
@@ -1,10 +1,10 @@
 #pragma once
 #include<algorithm>
+#include<stdexcept>
 
 template <typename T>
 class DynamicArray
 {
-private:
     T*data;
     int size;
 
@@ -39,5 +39,38 @@ public:
 
     ~DynamicArray() {
         delete[] data;
+    }
+
+    const T& Get(int index) {
+        if (index < 0 || index >= size) {
+            throw std::out_of_range("index out of range");
+        }
+
+        return data[index];
+    }
+
+    void Set(int index, T value) {
+        if (index < 0 || index >= size) {
+            throw std::out_of_range("index out of range");
+        }
+
+        data[index] = value;
+    }
+
+    [[nodiscard]] int GetSize() const {
+        return size;
+    }
+
+    void Resize(const int new_size) {
+        if (new_size < 0) {
+            throw std::invalid_argument("size must be positive");
+        }
+
+        T* new_data = new T[new_size];
+        int elements_to_copy = std::min(size, new_size);
+        std::copy(data, data + elements_to_copy, new_data);
+        delete[] data;
+        data = new_data;
+        size = new_size;
     }
 };
