35849e0012dc74a0bc6f5a281779eaca3175110e
dudukin matvei <matduduk@gmail.com>
2025-07-10 04:57:29 +0300

    setuped user interface

--- src/main.cpp
+++ src/main.cpp
@@ -4,35 +4,161 @@
 #include "../include/ArraySequence/Mutable/MutableArraySequence.h"
 #include "../include/ListSequence/Immutable/ImmutableListSequence.h"
 #include "../include/ListSequence/Mutable/MutableListSequence.h"
+#include <vector>
 
-template<typename T>
-void printSequence(const Sequence<T>* seq) {
-    std::cout << "Значения Sequence" << std::endl;
+void printSequence(const Sequence<int>* seq) {
+    std::cout << "\n";
     for (int i = 0; i < seq->GetLength(); ++i) {
         std::cout << seq->Get(i) << " ";
     }
     std::cout << "\n";
 }
 
-template<typename T>
-void inputSequence(Sequence<T>* seq, int size) {
-    std::cout << "Введите значения(через пробел)";
-    for (int i = 0; i < size; ++i) {
-        T item;
-        std::cin >> item;
-        seq->Append(item);
+void append(Sequence<int>* seq, int value, bool isMutable) {
+    if (isMutable) {
+        seq->Append(value);
+        printSequence(seq);
     }
-    std::cout << "\n";
+    else {
+        auto* newSeq = seq->Append(value);
+        printSequence(newSeq);
+    }
+}
+
+void prepend(Sequence<int>* seq, int value, bool isMutable) {
+    if (isMutable) {
+        seq->Prepend(value);
+        printSequence(seq);
+    }
+    else {
+        auto* newSeq = seq->Prepend(value);
+        printSequence(newSeq);
+    }
+}
+
+void insert(Sequence<int>* seq, int value, int index, bool isMutable) {
+    if (isMutable) {
+        seq->InsertAt(value, index);
+        printSequence(seq);
+    }
+    else {
+        auto* newSeq = seq->InsertAt(value, index);
+        printSequence(newSeq);
+    }
+}
+
+void get(Sequence<int>* seq, int index) {
+    std::cout << seq->Get(index) << "\n";
+}
+
+void getSub(Sequence<int>* seq, int start, int end) {
+    auto* subbed = seq->GetSubsequence(start, end);
+    printSequence(subbed);
+}
+
+void concat(Sequence<int>* seq1, Sequence<int>* seq2) {
+    printSequence(seq1->Concat(seq2));
+}
+
+void printSequenceActions() {
+    std::cout << "1. Append" << std::endl;
+    std::cout << "2. Prepend" << std::endl;
+    std::cout << "3. Insert" << std::endl;
+    std::cout << "4. Get" << std::endl;
+    std::cout << "5. GetSubSequence" << std::endl;
+    std::cout << "6. Concat with 1,2,3" << std::endl;
+    std::cout << "7. Output" << std::endl;
+    std::cout << "8. Exit" << std::endl;
+}
+
+void printSequenceChoices() {
+    std::cout << "Choose sequence to create:" << std::endl;
+    std::cout << "1. Mutable array" << std::endl;
+    std::cout << "2. Immutable array" << std::endl;
+    std::cout << "3. Mutable list" << std::endl;
+    std::cout << "4. Immutable list" << std::endl;
 }
 
 
-
-
-
 int main() {
-    std::string command;
-    while (command != "exit") {
+    int seqChoice;
+    Sequence<int>* seq = nullptr;
+    Sequence<int>* seqForConcat = nullptr;
+    int values[3] = {1, 2, 3};
+    bool isMutable;
+    printSequenceChoices();
+    std::cin >> seqChoice;
+    switch (seqChoice) {
+        case 1:
+            seq = new MutableArraySequence<int>();
+            seqForConcat = new MutableArraySequence<int>(values, 3);
+            isMutable = true;
+            break;
+        case 2:
+            seq = new ImmutableArraySequence<int>();
+            seqForConcat = new ImmutableArraySequence<int>(values, 3);
+            isMutable = false;
+            break;
+        case 3:
+            seq = new MutableListSequence<int>();
+            seqForConcat = new MutableListSequence<int>(values, 3);
+            isMutable = true;
+            break;
+        case 4:
+            seq = new ImmutableListSequence<int>();
+            seqForConcat = new ImmutableListSequence<int>(values, 3);
+            isMutable = false;
+            break;
+        default:
+            return 1;
+
+    }
+    int choice;
+    printSequenceActions();
+    while (std::cin >> choice && choice != 8) {
+        int value;
+        int index;
+        int end;
+        switch (choice) {
+            case 1:
+                std::cout << "Enter value to append: ";
+                std::cin >> value;
+                append(seq, value, isMutable);
+                break;
+            case 2:
+                std::cout << "Enter value to prepend: ";
+                std::cin >> value;
+                prepend(seq, value, isMutable);
+                break;
+            case 3:
+                std::cout << "Enter pair index/value to insert: ";
+                std::cin >> index >> value;
+                insert(seq, value, index, isMutable);
+                break;
+            case 4:
+                std::cout << "Enter index to get: ";
+                std::cin >> index;
+                get(seq, index);
+                break;
+            case 5:
+                std::cout << "Enter start, end indexes to getSubsequence: ";
+                std::cin >> index >> end;
+                getSub(seq, index, end);
+                break;
+            case 6:
+                concat(seq, seqForConcat);
+                break;
+            case 7:
+                printSequence(seq);
+                break;
+            default:
+                std::cout << "Unknown choice." << std::endl;
+
+        }
+        printSequenceActions();
     }
 
+    delete seq;
+    delete seqForConcat;
     return 0;
 }
